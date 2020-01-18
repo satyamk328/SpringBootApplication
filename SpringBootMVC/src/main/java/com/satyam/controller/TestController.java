04f9de3b4453b0b399ba3b3a9a27a27ad889b1fb
Satyam Kumar <satyamk328@gmail.com>
2020-01-18 18:04:13 +0530

    aDASD

---
+++ SpringBootMVC/src/main/java/com/satyam/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.satyam.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView firstPage() {
+		return new ModelAndView("welcome");
+	}
+
+}
